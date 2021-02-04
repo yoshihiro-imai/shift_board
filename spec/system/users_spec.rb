59997340bdda1f435f030f30d88c0bb58a46ee01
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2021-02-04 19:40:43 +0900

    結合テスト　（ユーザー管理機能）

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+def basic_pass(path) #---❶
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      basic_pass root_path
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in '名前' ,with: @user.name
+      fill_in 'メールアドレス',with: @user.email
+      fill_in 'パスワード',with: @user.password
+      fill_in 'パスワード（確認用）',with: @user.password_confirmation
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name= "commit"]').click
+      }.to change { User.count}.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq(root_path)
+      # ログアウトボタンが表示されることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規投稿')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+    basic_pass root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in '名前' ,with: ''
+      fill_in 'メールアドレス',with: ''
+      fill_in 'パスワード',with: ''
+      fill_in 'パスワード（確認用）',with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change{ User.count}.by(0)
+    end
+  end
+end
