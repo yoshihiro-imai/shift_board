b3c8edb4f04fc0c107ae414a90aadaa6898f321f
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2021-02-04 13:03:54 +0900

    修正

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :tasks
+  has_many :tasks ,foreign_key: :project_id, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
   validates :nickname, presence: true
