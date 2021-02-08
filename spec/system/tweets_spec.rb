004ccf3efbdea756ccb9337340a3c31619796493
yoshihiro-imai <75416155+yoshihiro-imai@users.noreply.github.com>
2021-02-08 14:33:04 +0900

    結合テスト

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -52,3 +52,51 @@ RSpec.describe '掲示板投稿', type: :system do
     end
   end
 end
+
+  RSpec.describe 'ツイート編集', type: :system do
+    before do
+      @tweet1 = FactoryBot.create(:tweet)
+      @tweet2 = FactoryBot.create(:tweet)
+    end
+    context 'ツイート編集ができるとき' do
+      it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
+        basic_pass root_path
+
+        # ツイート1を投稿したユーザーでログインする
+        visit new_user_session_path
+        fill_in 'メールアドレス', with:@tweet1.user.email
+        fill_in 'パスワード', with: @tweet1.user.password
+        find('input[name="commit"]').click
+        expect(current_path).to eq(root_path)
+        # ツイート1に「編集」ボタンがあることを確認する
+      
+        
+        # 編集ページへ遷移する
+        visit edit_tweet_path(@tweet1)
+        # すでに投稿済みの内容がフォームに入っていることを確認する
+        expect(
+          find('#tweet_title').value
+        ).to eq(@tweet1.title)
+        expect(
+          find('#tweet_context').value
+        ).to eq(@tweet1.context)
+        # 投稿内容を編集する
+        fill_in 'tweet_title', with: "#{@tweet1.title}+編集したタイトル"
+        fill_in 'tweet_context', with: "#{@tweet1.context}+編集したテキスト"
+        # 編集してもTweetモデルのカウントは変わらないことを確認する
+        expect{
+          find('input[name="commit"]').click
+      }.to change{Tweet.count}.by(0)
+        # 編集完了画面に遷移したことを確認する
+        expect(current_path).to eq(tweet_path(@tweet1))
+        # 「更新が完了しました」の文字があることを確認する
+        expect(page).to have_content('更新が完了しました')
+        # トップページに遷移する
+        visit root_path
+        # 先ほど変更した内容のツイートが存在することを確認する（テキスト）
+        visit tweet_path(@tweet1)
+        expect(page).to have_content("#{@tweet1.title}+編集したタイトル")
+      end
+    end
+    
+end
